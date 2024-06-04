2913c95ccd780aece2e7473f7498ebea9b335424
lequyan2003 <lequyan2003.com>
2024-06-04 16:01:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useEffect } from "react";
+// import { useState } from "react";
+import { useFetch } from "./hooks/useFetch";
+// import { useReducer } from "./hooks/useReducer";
+
+// function fetchReducer(state, action) {
+//   switch(action.type) {
+//     case 'fetchAPI/request':
+//       return { ...state, isLoading: action.isLoading };
+//     case 'fetchAPI/success':
+//       return {
+//         ...state, 
+//         isLoading: action.isLoading, 
+//         error: action.error, 
+//         data: action.data 
+//       };
+//     case 'fetchAPI/error':
+//       return {
+//         ...state, 
+//         isLoading: action.isLoading, 
+//         error: action.error, 
+//         data: action.data 
+//       };
+//     default:
+//       return state;
+//   }
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // const [state, dispatch] = useReducer(fetchReducer, {
+  //   data: [],
+  //   isLoading: false,
+  //   error: null
+  // });
+
+  // const { data: users, isLoading, error } = state;
+  // const [users, setUsers] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
+
+  // use
+  // const fetchUsers = async () => {
+  //   //setIsLoading(true);
+  //   dispatch({
+  //     type: 'fetchAPI/request',
+  //   });
+  //   try {
+  //     const res = await fetch('https://reqres.in/api/users');
+  //     const { data } = await res.json();
+
+  //     // setUsers(data);
+  //     // setError(null);
+  //     // setIsLoading(false);
+  //     dispatch({
+  //       type: 'fetchAPI/success',
+  //       isLoading: false,
+  //       error: null,
+  //       data
+  //     });
+  //   } catch (err) {
+  //     // setIsLoading(false);
+  //     // setError(err);
+  //     // setUsers([]);
+  //     dispatch({
+  //       type: 'fetchAPI/error',
+  //       isLoading: false,
+  //       error: null,
+  //       data: []
+  //     });
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchUsers();
+  // }, []);
+
+  const {
+    data: users, isLoading, error
+  } = useFetch('https://reqres.in/api/users');
+
+  if (error) {
+    return <p>Something went wrong!!!</p>;
+  }
+
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    users.map((user) => (
+      <p>
+        {user.first_name} {user.last_name}
+      </p>
+    ))
   );
 }
 
